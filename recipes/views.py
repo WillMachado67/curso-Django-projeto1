821d34cf3940a4a7083d25116b91b3f5f0d810e6
Willian_Machado <willian.machado.av@gmail.com>
2023-02-21 10:34:00 -0300

    Meu primeiro projeto django

---
+++ recipes/views.py
@@ -0,0 +1,18 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'recipes/home.html', status=201, context={
+        'name': 'Willian Machado'
+    })
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
