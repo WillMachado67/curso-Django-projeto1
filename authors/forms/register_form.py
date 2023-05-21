71cfb72379a8ca7ebc60bcf57c42e0895afcd833
Willian_Machado <willian.machado.av@gmail.com>
2023-05-21 14:02:01 -0300

    Dashboard do usuario criado

--- authors/forms/register_form.py
+++ authors/forms/register_form.py
@@ -74,22 +74,6 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
-        # labels = {
-        #     'username': 'Username',
-        #     'first_name': 'First name',
-        #     'last_name': 'Last name',
-        #     'email': 'E-mail',
-        #     'password': 'Password',
-        # }
-        # help_texts = {
-        #     'email': 'The e-mail must be valid.'
-        # }
-
-        # error_messages = {
-        #     'username': {
-        #         'required': 'This field must not be enpty'
-        #     }
-        # }
 
     def clean_email(self):
         email = self.cleaned_data.get('email', '')
